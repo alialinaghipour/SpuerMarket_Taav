dfde7f0f9f78dbf0783b07d302e53d5375cc565f
alimmst <79074714+alimmst@users.noreply.github.com>
2021-03-13 16:30:13 +0330

    Add ProductCategoryEntityMap

---
+++ SuperMarket/SupeMarket.Persistence.EF/ProductCategories/ProductCategoryEntityMap.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SuperMarket.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SupeMarket.Persistence.EF.ProductCategories
+{
+    public class ProductCategoryEntityMap : IEntityTypeConfiguration<ProductCategory>
+    {
+        public void Configure(EntityTypeBuilder<ProductCategory> builder)
+        {
+            builder.ToTable("ProductCategories");
+
+            builder.HasKey(_ => _.Id);
+
+            builder.Property(_ => _.Id).ValueGeneratedOnAdd();
+
+            builder.Property(_ => _.Tilte).IsRequired().HasMaxLength(40);
+        }
+    }
+}
