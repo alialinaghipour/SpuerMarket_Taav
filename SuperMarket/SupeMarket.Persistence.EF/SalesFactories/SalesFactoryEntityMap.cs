b39a42258d893e1c7890376c15c0f519ab3f6dcb
alimmst <79074714+alimmst@users.noreply.github.com>
2021-03-14 09:37:55 +0330

    Add SalesFactory EntityMap

---
+++ SuperMarket/SupeMarket.Persistence.EF/SalesFactories/SalesFactoryEntityMap.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SuperMarket.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SupeMarket.Persistence.EF.SalesFactories
+{
+    public class SalesFactoryEntityMap : IEntityTypeConfiguration<SalesFactor>
+    {
+        public void Configure(EntityTypeBuilder<SalesFactor> builder)
+        {
+            builder.ToTable("SalesFactors");
+
+            builder.HasKey(_ => _.Id);
+
+            builder.Property(_ => _.Id).ValueGeneratedOnAdd();
+
+            builder.Property(_ => _.ProductCode).IsRequired().HasMaxLength(10);
+
+            builder.Property(_ => _.Count).IsRequired();
+
+            builder.Property(_ => _.SalesDate).IsRequired().HasDefaultValue(DateTime.Now);
+        }
+    }
+}
