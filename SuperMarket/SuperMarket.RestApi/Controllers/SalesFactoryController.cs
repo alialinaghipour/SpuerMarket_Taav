40d35512d8a8bb966f181960bfd21b00c853e987
alimmst <79074714+alimmst@users.noreply.github.com>
2021-03-14 11:52:37 +0330

    Add Contoroller SalesFactory

---
+++ SuperMarket/SuperMarket.RestApi/Controllers/SalesFactoryController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SuperMarket.Services.SalesFactories;
+
+namespace SuperMarket.RestApi.Controllers
+{
+    [ApiController,Route("api/sales-factory")]
+    public class SalesFactoryController : Controller
+    {
+        private readonly SalesFactoryServices _services;
+
+        public SalesFactoryController(SalesFactoryServices services)
+        {
+            _services = services;
+        }
+
+        [HttpPost]
+        public void Add(AddSalesFactorDto dto)
+        {
+            _services.Add(dto);
+        }
+
+        [HttpGet]
+        public IList<GetAllSalesFactoryDto> GetAll()
+        {
+            return _services.GetAll();
+        }
+
+        [HttpGet("{id}")]
+        public GetByIdSalesFactoryDto GetById(int id)
+        {
+            return _services.GetById(id);
+        }
+    }
+}
